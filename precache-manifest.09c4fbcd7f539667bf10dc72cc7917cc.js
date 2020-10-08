58a91c304e53bb718b7b28e7f7e94ab0c8146f58
David Khayutin <davidkhayutin@Davids-MacBook-Pro-2.local>
2020-10-08 12:24:18 -0700

    thing

---
+++ precache-manifest.09c4fbcd7f539667bf10dc72cc7917cc.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "927071d3830aa6d628d1",
+    "url": "/static/js/widget.js"
+  },
+  {
+    "revision": "927071d3830aa6d628d1",
+    "url": "/static/css/widget.css"
+  },
+  {
+    "revision": "ddf767c986405a41d56538aa8ef3054c",
+    "url": "/index.html"
+  }
+];
